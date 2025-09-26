342467ff35334d57a16b540758c76b42a15be75d
mrmakrider <107931983+mrmakrider@users.noreply.github.com>
2025-09-26 20:14:24 +0300

    Create build.gradle.kts

---
+++ TukTukCinemaProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = "1.0.0"
+
+cloudstream {
+    language = "ar"
+    authors = listOf("YourName")
+    status = 1
+    
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie", 
+        "Anime",
+        "AsianDrama"
+    )
+
+    iconUrl = "https://tuk.cam/favicon.ico"
+}
